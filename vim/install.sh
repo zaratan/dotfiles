2e3fa2c0fee0aadb09a9b7a65cb6d1c7c315c49b
Denis Zaratan Pasin <denis.pasin@autodesk.com>
2019-11-18 09:50:25 +0900

    Don't use vundle anymore

--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,4 @@
 pip3 install unidecode neovim
-if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
 if [[ ! -d ~/.vim/undo ]]; then
   mkdir ~/.vim/undo
 fi
