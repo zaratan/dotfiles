bc407c75ff9cb76e0a861e7808a199d79c12bdc7
Denis Zaratan Pasin <denis.pasin@gmail.com>
2019-12-14 17:15:38 +0100

    Big update

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,5 +1,5 @@
 if [[ ! -d ~/.rvm ]]; then
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-  \curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.1
+  \curl -sSL https://get.rvm.io | bash -s stable --ruby=2.6.5
   gem install bundler
 fi
--- vim/install.sh
+++ vim/install.sh
@@ -2,3 +2,7 @@ pip3 install unidecode neovim
 if [[ ! -d ~/.vim/undo ]]; then
   mkdir ~/.vim/undo
 fi
+if [[ ! -d ~/.config/nvim ]]; then
+  mkdir -p ~/.config/nvim
+fi
+ln -s ./vimrc.symlink ~/.config/nvim/init.vim
