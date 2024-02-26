8ec067d8aa5398aac8eb613e6d8097f915db73b9
Denis Zaratan Pasin <zaratan@hey.com>
2024-02-26 11:47:18 +0100

    Nico haz arrived

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,5 +1,5 @@
 if [[ ! -d ~/.rvm ]]; then
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-  \curl -sSL https://get.rvm.io | bash -s stable --ruby=2.7.1
+  \curl -sSL https://get.rvm.io | bash -s stable --ruby=3.0.2
   gem install bundler
 fi
