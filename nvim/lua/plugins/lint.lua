0d378f00e2df220018d9528607d4230790a98769
Denis Zaratan Pasin <zaratan@hey.com>
2025-12-08 09:44:33 +0100

    Asdf

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -20,11 +20,13 @@ return {
           lua = { "stylua" },
         },
 
-        format_on_save = {
-          -- These options will be passed to conform.format()
-          timeout_ms = 1000,
-          lsp_format = "fallback",
-        },
+        format_on_save = function(bufnr)
+          -- Disable with a global or buffer-local variable
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 1000, lsp_format = "fallback" }
+        end,
       })
       vim.api.nvim_create_user_command("Format", function(args)
         local range = nil
@@ -37,6 +39,23 @@ return {
         end
         require("conform").format({ async = true, lsp_format = "fallback", range = range })
       end, { range = true })
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = "Disable autoformat-on-save",
+        bang = true,
+      })
+      vim.api.nvim_create_user_command("FormatEnable", function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = "Re-enable autoformat-on-save",
+      })
     end,
   },
 }
