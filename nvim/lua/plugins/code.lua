2a116cd8f2a5c1c983871471c655f42dce74a8d8
Denis Zaratan Pasin <zaratan@hey.com>
2026-04-09 10:17:33 +0200

    Modern Tree sitter

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -22,46 +22,55 @@ return {
     cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
 
     config = function()
-      local configs = require("nvim-treesitter.config")
+      require("nvim-treesitter").install({
+        "bash",
+        "c",
+        "comment",
+        "css",
+        "diff",
+        "embedded_template",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "latex",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "prisma",
+        "printf",
+        "python",
+        "query",
+        "regex",
+        "ruby",
+        "rust",
+        "sql",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      })
 
-      configs.setup({
-        ensure_installed = {
-          "bash",
-          "c",
-          "comment",
-          "css",
-          "diff",
-          "embedded_template",
-          "html",
-          "javascript",
-          "jsdoc",
-          "json",
-          "jsonc",
-          "latex",
-          "lua",
-          "luadoc",
-          "luap",
-          "markdown",
-          "markdown_inline",
-          "prisma",
-          "printf",
-          "python",
-          "query",
-          "regex",
-          "ruby",
-          "rust",
-          "sql",
-          "toml",
-          "tsx",
-          "typescript",
-          "vim",
-          "vimdoc",
-          "xml",
-          "yaml",
-        },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
+      -- On the `main` branch of nvim-treesitter, highlight/indent must be
+      -- enabled manually via an autocmd. This also enables language injections
+      -- (e.g. SQL/HTML inside Ruby heredocs).
+      -- Map filetypes that don't have their own parser to an existing one.
+      vim.treesitter.language.register("json", "jsonc")
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          local ft = vim.bo[args.buf].filetype
+          local lang = vim.treesitter.language.get_lang(ft) or ft
+          if pcall(vim.treesitter.language.add, lang) then
+            pcall(vim.treesitter.start, args.buf)
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
       })
     end,
   },
