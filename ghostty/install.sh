749f2c508dd6e93f9e60ac0ee84abb181cee141a
Denis Zaratan Pasin <zaratan@hey.com>
2026-07-13 07:51:39 +0200

    Stow

--- ghostty/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-if [ -d "$HOME/.config/ghostty" ]; then
-    echo "Directory $HOME/.config/ghostty already exists."
-else
-  ln -s "$PWD/ghostty" "$HOME/.config/ghostty"
-fi
--- nvim/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-if [ -d "$HOME/.config/nvim" ]; then
-    echo "Directory $HOME/.config/nvim already exists."
-else
-  ln -s "$PWD/nvim" "$HOME/.config/nvim"
-fi
