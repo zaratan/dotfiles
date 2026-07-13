bd52679e09b5d35a88b9f6c731262bb4f751ab1d
Denis Zaratan Pasin <zaratan@hey.com>
2026-07-13 11:00:42 +0200

    setup script

--- install/macos-defaults.sh
+++ install/macos-defaults.sh
@@ -1,38 +1,33 @@
-# Sets reasonable macOS defaults.
-#
-# Or, in other words, set shit how I like in macOS.
-#
-# The original idea (and a couple settings) were grabbed from:
-#   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
-#
-# Run ./set-defaults.sh and you'll be good to go.
+#!/usr/bin/env bash
+# Defaults macOS raisonnables.
+# Quelques réglages inspirés de https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+set -euo pipefail
 
-# Disable press-and-hold for keys in favor of key repeat.
+# Désactive press-and-hold au profit de la répétition de touche.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-# Use AirDrop over every interface. srsly this should be a default.
+# AirDrop sur toutes les interfaces.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
-# Always open everything in Finder's list view. This is important.
+# Finder : toujours en vue liste.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
-# Show the ~/Library folder.
+# Affiche ~/Library.
 chflags nohidden ~/Library
 
-# Set a really fast key repeat.
+# Répétition de touche très rapide.
 defaults write NSGlobalDomain KeyRepeat -int 0
 
-# Set the Finder prefs for showing a few different volumes on the Desktop.
+# Volumes externes visibles sur le bureau.
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# Run the screensaver if we're in the bottom-left hot corner.
+# Écran de veille dans le coin bas-gauche, mot de passe immédiat.
 defaults write com.apple.dock wvous-bl-corner -int 5
 defaults write com.apple.dock wvous-bl-modifier -int 0
-
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-# Disable smart quotes and dashes
+# Désactive guillemets et tirets « intelligents ».
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
