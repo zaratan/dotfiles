bee0e587ad7ef2ef83424fcd13d593adf5516d43
Denis Zaratan Pasin <zaratan@hey.com>
2026-07-13 06:57:27 +0200

    Remove dead topics, stale bin scripts and Holman-era leftovers

--- kitty/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-if [ -d "$HOME/.config/kitty" ]; then
-    echo "Directory $HOME/.config/kitty already exists."
-else
-  ln -s "$PWD/kitty" "$HOME/.config/kitty"
-fi
--- zsh/install.sh
+++
@@ -1,3 +0,0 @@
-if [[ ! -d ~/.oh-my-zsh/custom/plugins ]]; then
-  cp -r zsh/custom_plugins/* ~/.oh-my-zsh/custom/plugins/
-fi
